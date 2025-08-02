751286986dea3ecef0b66d9f8a8d6842ace35421
Lucas Castro <lucasamonrc@gmail.com>
2025-08-02 14:23:58 -0500

    Set up base layouts

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="mt-16 sm:text-center text-sm">
+      Copyright &copy; {new Date().getFullYear()} Lucas Castro. All rights
+      reserved.
+    </footer>
+  );
+}
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Lucas Castro";
+export const SITE_DESCRIPTION =
+  "Hello, there! I'm Lucas. Welcome to my personal website. You can learn more about me, my work, and the things I care about here. I talk about programming, careers, and the tech industry.";
