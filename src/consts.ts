fb4308988a014e94e2b908ae92c346b50477ddee
Lucas Castro <53352488+lucasamonrc@users.noreply.github.com>
2025-10-09 12:13:54 -0500

    Add new talk

--- src/consts.ts
+++ src/consts.ts
@@ -44,6 +44,12 @@ export const WORK = [
 
 export const TALKS = [
 	{
+		date: "Sep. 12, 2025",
+		title: "Managing a State of Distrust",
+		videoUrl: "https://www.youtube.com/watch?v=ZBVpyTUw1ZE",
+		notesUrl: "https://lucasamonrc.notion.site/Managing-a-State-of-Distrust-24d0becdfd4c8055a45ddb3d3eca4104"
+	},
+	{
 		date: "Mar. 5, 2025",
 		title: "Auth 101",
 		videoUrl: "https://www.youtube.com/watch?v=Z8iFDkBRfWI",
